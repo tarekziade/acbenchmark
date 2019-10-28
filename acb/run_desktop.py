2723dfe392038dc829aec5b981eb939a8a26916c
Tarek Ziade <tarek@ziade.org>
2019-10-28 18:09:02 +0100

    exposed prefs

--- acb/run_desktop.py
+++ acb/run_desktop.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-
+import json
 
 _REP = (
     ("http://", ""),
@@ -19,11 +19,10 @@ def result_dir(url, test_name):
         url = url.replace(orig, repl)
     return os.path.join("browsertime-results", url, test_name)
 
+# find out the local host ip automatically
+# run "co servers" prior to running the script.
+# I guess we can provide them using a list of commands, one per coserver/
 
-host_ip = "127.0.0.1"
-# firefox_args += '--firefox.geckoProfiler true --firefox.geckoProfilerParams.interval 10   --firefox.geckoProfilerParams.features "js,stackwalk,leaf" --firefox.geckoProfilerParams.threads "GeckoMain,Compositor,ssl,socket,url,bhm,dns" '
-# WebPageReplay
-# firefox_args += '--firefox.preference network.dns.forceResolve:' + host_ip + ' --firefox.acceptInsecureCerts true '
 
 HERE = os.path.dirname(__file__)
 
@@ -49,7 +48,15 @@ def main():
     parser = argparse.ArgumentParser(description="acbenchmark")
     parser.add_argument("--iterations", type=int, default=20, help="Number of iterations")
     parser.add_argument("--name", default="tinap", help="Name of the test")
+    parser.add_argument("--prefs", default=None, help="prefs file")
     args = parser.parse_args()
+
+    if os.path.exists(args.prefs):
+        with open(args.prefs) as f:
+            prefs = json.loads(f.read())
+        for key, value in prefs.items():
+            _CMD.append("--%s" % key)
+            _CMD.append(str(value))
 
     for line in open(SITES_TXT):
         url = line.strip()
@@ -59,6 +66,7 @@ def main():
             "url": url,
             "iterations": args.iterations,
             "result_dir": result_dir(url, args.name),
+            "host_ip": "127.0.0.1"
         }
         cmd = " ".join(_CMD) % options
         print("\ncommand " + cmd)
